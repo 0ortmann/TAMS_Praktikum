f992340741fe4766bcd858f1bdd95383c805ccde
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-05-11 15:18:58 +0200

    extract increment func; alarm

---
+++ ex3/alarm.py
@@ -0,0 +1,39 @@
+from myhdl import delay, always, instance, Signal, intbv, always_seq
+from incrementor import increment
+
+
+def Alarm(alarm_mins1, alarm_mins10, alarm_hrs1, alarm_hrs10, clk500ms, set_alarm, set_hrs, set_mins, reset):
+    """
+    General Alarm Block
+    :param alarm_mins1: alarm minute output
+    :param alarm_mins10: alarm 10th-minute output
+    :param alarm_hrs1: alarm hour output
+    :param alarm_hrs10: alarm 10th-hour output
+    :param clk500ms: 500ms Clock Input
+    :param set_alarm: alarm set input
+    :param set_mins: alarm minute input
+    :param set_hrs: alarm hour input
+    :param reset: Reset input
+    :return:
+    """
+
+
+    @instance
+    def clock():
+        while True:
+            if not reset:
+                print "reset"
+                alarm_hrs1.next = 0
+                alarm_hrs10.next = 0
+                alarm_mins1.next = 0
+                alarm_mins10.next = 0
+            if set_alarm:
+                print "set alarm"
+                yield clk500ms.posedge, reset
+                if set_hrs:
+                    increment(alarm_hrs1, alarm_hrs10, intbv(2), intbv(3))
+                elif set_mins:
+                    increment(alarm_mins1, alarm_mins10, intbv(6), intbv(9))
+            else:
+                yield set_alarm.posedge, reset
+    return clock
--- ex3/timer.py
+++ ex3/timer.py
@@ -1,38 +1,20 @@
 from myhdl import delay, always, instance, Signal, intbv, always_seq
-
+from incrementor import increment
 
 def Timer(time_secs1, time_secs10, time_mins1, time_mins10, time_hrs1, time_hrs10, clk1s, clk500ms, set_time, set_hrs, set_mins, reset):
     """
     General Clock Driver
+    :param time_secs1: time seconds output
+    :param time_secs10: time seconds output
+    :param time_mins1: time minute output
+    :param time_mins10: time 10th-minute output
+    :param time_hrs1: time hour output
+    :param time_hrs10: time 10th-hour output
     :param clk1s: 1s Clock Output
     :param clk500ms: 500ms Clock Output
-    :param clk1ms:  1ms Clock Output
-    :param clk1us: Clock input (1us)
     :param reset: Reset input
     :return:
     """
-
-    def increment(lower, upper, max_upper, max_lower_on_upper):
-        """
-        Increments a two secment timer, with lower and upper the counters
-        :param lower: the lower counter
-        :param upper: the upper counter
-        :param max_upper: the max value for the upper register
-        :param max_lower_on_upper: the max value for the lower register if the upper register is max
-        :return: bool if the counter as had an overflow in the last counting step
-        """
-        #a = intbv(9,min=0,max=10)
-        #a = intbv(9 if upper != max_upper else max_lower_on_upper, min=0, max=10)
-        if 9 == lower or (upper == max_upper and lower == max_lower_on_upper):
-            upper.next = upper + 1 if upper < max_upper else 0
-        if upper == max_upper:
-            lower.next = lower + 1 if lower < max_lower_on_upper else 0
-        else:
-            lower.next = lower + 1 if lower < 9 else 0
-
-    """ def is_overflow(lower, upper, max_upper, max_lower_on_upper):
-        a = intbv(9 if upper != max_upper else max_lower_on_upper, min=0, max=10)
-        return upper == max_upper and lower == a"""
 
     @instance
     def clock():
