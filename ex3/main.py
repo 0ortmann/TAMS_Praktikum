7e4a4f4390b580edce2a80db57f8d95defd92e77
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2016-05-11 15:30:33 +0200

    simulate set_alarm

--- ex3/main.py
+++ ex3/main.py
@@ -2,6 +2,7 @@
 
 from clock import ClkDriver, Clock
 from timer import Timer
+from alarm import Alarm
 
 from myhdl import Signal, toVHDL, traceSignals, Simulation, ResetSignal, toVerilog, intbv
 
@@ -12,29 +13,38 @@ if __name__ == "__main__":
     clk1ms = Signal(bool(0))
     clk500ms = Signal(bool(0))
     clk1s = Signal(bool(0))
-    set_time = Signal(bool(1))
-    set_hrs = Signal(bool(0))
-    set_mins = Signal(bool(1))
+    set_time = Signal(bool(0))
+    set_ala = Signal(bool(1))
+    set_hrs = Signal(bool(1))
+    set_mins = Signal(bool(0))
     tim_secs1 = Signal(intbv(7, min=0, max=10))
     tim_secs10 = Signal(intbv(5, min=0, max=6))
     tim_mins1 = Signal(intbv(9, min=0, max=10))
     tim_mins10 = Signal(intbv(5, min=0, max=6))
     tim_hrs1 = Signal(intbv(3, min=0, max=10))
     tim_hrs10 = Signal(intbv(2, min=0, max=3))
+    ala_mins1 = Signal(intbv(9, min=0, max=10))
+    ala_mins10 = Signal(intbv(5, min=0, max=6))
+    ala_hrs1 = Signal(intbv(3, min=0, max=10))
+    ala_hrs10 = Signal(intbv(2, min=0, max=3))
 
     clkDriver = ClkDriver(clk1us)
 
     clock = Clock(clk1s, clk500ms, clk1ms, clk1us, reset)
 
     timblk = Timer(tim_secs1, tim_secs10, tim_mins1, tim_mins10, tim_hrs1, tim_hrs10, clk1s, clk500ms, set_time, set_hrs, set_mins, reset)
-
-    sim = Simulation(clkDriver, clock, timblk)
+    alarm = Alarm(ala_mins1, ala_mins10, ala_hrs1, ala_hrs10, clk500ms, set_ala, set_hrs,
+           set_mins, reset)
+    sim = Simulation(clkDriver, clock, timblk, alarm)
 
     toVHDL(Clock, clk1s, clk500ms, clk1ms, clk1us, reset)
     toVHDL(Timer, tim_secs1, tim_secs10, tim_mins1, tim_mins10, tim_hrs1, tim_hrs10, clk1s, clk500ms, set_time, set_hrs, set_mins, reset)
-    toVerilog(Timer, tim_secs1, tim_secs10, tim_mins1, tim_mins10, tim_hrs1, tim_hrs10, clk1s, clk500ms, set_time, set_hrs, set_mins, reset)
-    toVerilog(Clock, clk1s, clk500ms, clk1ms, clk1us, reset)
+    toVHDL(Alarm, ala_mins1, ala_mins10, ala_hrs1, ala_hrs10, clk500ms, set_ala, set_hrs, set_mins, reset)
+    #toVerilog(Timer, tim_secs1, tim_secs10, tim_mins1, tim_mins10, tim_hrs1, tim_hrs10, clk1s, clk500ms, set_time, set_hrs, set_mins, reset)
+    #toVerilog(Clock, clk1s, clk500ms, clk1ms, clk1us, reset)
     #traceSignals(Clock, clk1s, clk500ms, clk1ms, clk1us, reset)
-    traceSignals(Timer, tim_secs1, tim_secs10, tim_mins1, tim_mins10, tim_hrs1, tim_hrs10, clk1s, clk500ms, set_time, set_hrs,
+    #traceSignals(Timer, tim_secs1, tim_secs10, tim_mins1, tim_mins10, tim_hrs1, tim_hrs10, clk1s, clk500ms, set_time, set_hrs,
+    #   set_mins, reset)
+    traceSignals(Alarm, ala_mins1, ala_mins10, ala_hrs1, ala_hrs10, clk500ms, set_ala, set_hrs,
            set_mins, reset)
     sim.run(5 * 1000000*1000)
