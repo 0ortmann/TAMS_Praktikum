5488cfd5d4c7dbca76b2bb92338f901b96ef0667
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-06-01 16:42:35 +0200

    try things as class

--- ex3/outmux.py
+++ ex3/outmux.py
@@ -1,27 +1,6 @@
 from myhdl import delay, always, instance, Signal, intbv, always_seq
 import sevenSegment
 
-code = [None] * 10
-for key, val in sevenSegment.encoding.items():
-    if 0 <= key <= 9:
-        code[key] = int(val, 2)
-code = tuple(code)
-
-
-def bcd2led(led, bcd):
-    led.next = code[int(bcd)]
-
-
-def incrementInBounds(bitv):
-    bitv.next = bitv + 1 if bitv < bitv.max -1 else 0
-
-def signalToPlainBV(signal):
-    bv = intbv(0, min=0, max=64)
-    bv[signal] = 1
-    return bv
-
-
-
 def Outmux(sev_seg_digit, select_digit,
            time_secs1, time_secs10, time_mins1, time_mins10, time_hrs1, time_hrs10, alarm_mins1, alarm_mins10,
            alarm_hrs1, alarm_hrs10, set_alarm, clk1ms, reset):
@@ -47,6 +26,27 @@ def Outmux(sev_seg_digit, select_digit,
 
     current_time_digit = Signal(intbv(0, min=0, max=6))
     current_alarm_digit = Signal(intbv(0, min=0, max=4))
+    
+    def __init__():
+        self.code = [None] * 10
+        
+        for key, val in sevenSegment.encoding.items():
+            if 0 <= key <= 9:
+                self.code[key] = int(val, 2)
+        self.code = tuple(self.code)
+
+
+    def bdc2led(self, led, bcd):
+        led.next = self.code[int(bcd)]
+
+
+    def incrementInBounds(self, bitv):
+        bitv.next = bitv + 1 if bitv < bitv.max -1 else 0
+
+    def signalToPlainBV(self, signal):
+        bv = intbv(0, min=0, max=64)
+        bv[signal] = 1
+        return bv
 
     @instance
     def multiplex():
@@ -58,34 +58,34 @@ def Outmux(sev_seg_digit, select_digit,
                 print "wait clock"
                 yield clk1ms.posedge, reset
                 print "display alarm"
-                incrementInBounds(current_alarm_digit)
+                self.incrementInBounds(current_alarm_digit)
                 if current_alarm_digit == 0:
-                    bcd2led(sev_seg_digit, alarm_hrs10)
+                    self.bdc2led(sev_seg_digit, alarm_hrs10)
                 elif current_alarm_digit == 1:
-                    bcd2led(sev_seg_digit, alarm_hrs1)
+                    self.bdc2led(sev_seg_digit, alarm_hrs1)
                 elif current_alarm_digit == 2:
-                    bcd2led(sev_seg_digit, alarm_mins10)
+                    self.bdc2led(sev_seg_digit, alarm_mins10)
                 elif current_alarm_digit == 3:
-                    bcd2led(sev_seg_digit, alarm_mins1)
-                select_digit.next = signalToPlainBV(current_alarm_digit)
+                    self.bdc2led(sev_seg_digit, alarm_mins1)
+                select_digit.next = self.signalToPlainBV(current_alarm_digit)
             else:
                 # display time
                 print "wait clock"
                 yield clk1ms.posedge, reset
                 print "display next"
-                incrementInBounds(current_time_digit)
+                self.incrementInBounds(current_time_digit)
                 if current_time_digit == 0:
-                    bcd2led(sev_seg_digit, time_hrs10)
+                    self.bdc2led(sev_seg_digit, time_hrs10)
                 elif current_time_digit == 1:
-                    bcd2led(sev_seg_digit, time_hrs1)
+                    self.bdc2led(sev_seg_digit, time_hrs1)
                 elif current_time_digit == 2:
-                    bcd2led(sev_seg_digit, time_mins10)
+                    self.bdc2led(sev_seg_digit, time_mins10)
                 elif current_time_digit == 3:
-                    bcd2led(sev_seg_digit, time_mins1)
+                    self.bdc2led(sev_seg_digit, time_mins1)
                 elif current_time_digit == 4:
-                    bcd2led(sev_seg_digit, time_secs10)
+                    self.bdc2led(sev_seg_digit, time_secs10)
                 elif current_time_digit == 5:
-                    bcd2led(sev_seg_digit, time_secs1)
-                select_digit.next = signalToPlainBV(current_time_digit)
+                    self.bdc2led(sev_seg_digit, time_secs1)
+                select_digit.next = self.signalToPlainBV(current_time_digit)
 
     return multiplex
