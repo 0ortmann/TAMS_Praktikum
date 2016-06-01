da8608fd33accf916bbb1f71b52473e0d31e21a0
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-06-01 15:37:48 +0200

    elifs

--- ex3/outmux.py
+++ ex3/outmux.py
@@ -39,10 +39,8 @@ def Outmux(sev_seg_digit, select_digit,
     :return:
     """
 
-    times = [time_hrs10, time_hrs1, time_mins10, time_mins1, time_secs10, time_secs1]
-    alarms = [alarm_hrs10, alarm_hrs1, alarm_mins10, alarm_mins1]
     current_time_digit = Signal(intbv(0, min=0, max=6))
-    current_alarm_digit = Signal(intbv(0, min=0, max=4))
+    current_alarm_digit = Signal(intbv(0, min=0, max=6))
 
     @instance
     def multiplex():
@@ -55,7 +53,14 @@ def Outmux(sev_seg_digit, select_digit,
                 yield clk1ms.posedge, reset
                 print "display alarm"
                 incrementInBounds(current_alarm_digit)
-                bcd2led(sev_seg_digit, alarms[current_alarm_digit])
+                if current_alarm_digit == 0:
+                    bcd2led(sev_seg_digit, alarm_hrs10)
+                elif current_alarm_digit == 1:
+                    bcd2led(sev_seg_digit, alarm_hrs1)
+                elif current_alarm_digit == 2:
+                    bcd2led(sev_seg_digit, alarm_mins10)
+                elif current_alarm_digit == 3:
+                    bcd2led(sev_seg_digit, alarm_mins1)
                 select_digit.next = current_alarm_digit
             else:
                 # display time
@@ -63,7 +68,18 @@ def Outmux(sev_seg_digit, select_digit,
                 yield clk1ms.posedge, reset
                 print "display next"
                 incrementInBounds(current_time_digit)
-                bcd2led(sev_seg_digit, times[current_time_digit])
+                if current_time_digit == 0:
+                    bcd2led(sev_seg_digit, time_hrs10)
+                elif current_time_digit == 1:
+                    bcd2led(sev_seg_digit, time_hrs1)
+                elif current_time_digit == 2:
+                    bcd2led(sev_seg_digit, time_mins10)
+                elif current_time_digit == 3:
+                    bcd2led(sev_seg_digit, time_mins1)
+                elif current_time_digit == 4:
+                    bcd2led(sev_seg_digit, time_secs10)
+                elif current_time_digit == 5:
+                    bcd2led(sev_seg_digit, time_secs1)
                 select_digit.next = current_time_digit
 
     return multiplex
